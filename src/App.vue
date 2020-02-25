f37d3822baa0ece5e75b59bf2dd683751d222517
Kevin <kphelan17@gmail.com>
2020-02-24 19:08:12 -0500

    change to welcome screen

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <v-app>
-
       <div>
        <v-app-bar dense>
-
          <v-toolbar-title>Sudoku</v-toolbar-title>
        </v-app-bar>
       </div>
-
-       
       <v-container >
         <v-row justify="center">
           <v-card-title>
             <h1 class="display-1" >Let's play some sudoku!</h1>
           </v-card-title>
         </v-row>
-        
-      <!--<v-btn color="success" > Register </v-btn>
-        //<v-btn color="info"> Login </v-btn>  
-        <v-row justify="center">
-        <v-btn primary color="success" class="mb-2">Start</v-btn>
-        </v-row>    
-      -->
       <v-row justify="center">
-      <!--    <board class="mt-1"/>   -->
         <router-view></router-view>
       </v-row>
       </v-container>
--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
     <h1>Validate your own board</h1>
-    <v-btn color="primary" to="/game/blank" >easy</v-btn>
+    <v-row justify="center">
+        <v-btn color="primary" to="/game/blank" >Empty</v-btn>
+    </v-row>
     <h1>Please pick a difficutly:</h1>
     <v-btn color="primary" to="/game/easy" >easy</v-btn>
     <v-btn color="primary" to="/game/medium">medium</v-btn>
